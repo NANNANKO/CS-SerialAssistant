bf10ef09c403b60e371c5eb9c4c0b0e32b53e637
xunan_wang <781325436@qq.com>
2021-05-11 12:03:50 +0800

    2021年5月11日12点03分

--- Form1.cs
+++ Form1.cs
@@ -26,11 +26,13 @@ namespace _2105Demo1
             //}
             string[] BandRate = { "9600", "115200", "230400", "2000000" };
             string[] DataBit = { "1", "2", "3", "4", "5", "6", "7", "8" };
-            
+            string[] StopBit = { "None", "Odd", "Even", "Mark", "Space" };
 
             comboBox1.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
             comboBox2.Items.AddRange(BandRate);
             comboBox3.Items.AddRange(DataBit);
+            comboBox4.Items.AddRange(StopBit);
+
 
             comboBox1.Text = "COM1";
             comboBox2.Text = "115200";
@@ -39,6 +41,84 @@ namespace _2105Demo1
             comboBox5.Text = "1";
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //将可能产生异常的代码放置在try块中
+                //根据当前串口属性来判断是否打开
+                if (serialPort1.IsOpen)
+                {
+                    //串口已经处于打开状态
+                    serialPort1.Close();    //关闭串口
+                    button1.Text = "打开串口";
+                    button1.BackColor = Color.ForestGreen;
+                    comboBox1.Enabled = true;
+                    comboBox2.Enabled = true;
+                    comboBox3.Enabled = true;
+                    comboBox4.Enabled = true;
+                    comboBox5.Enabled = true;
+                    textBox2.Text = "";  //清空接收区
+                    textBox1.Text = "";     //清空发送区
+                }
+                else
+                {
+                    //串口已经处于关闭状态，则设置好串口属性后打开
+                    comboBox1.Enabled = false;
+                    comboBox2.Enabled = false;
+                    comboBox3.Enabled = false;
+                    comboBox4.Enabled = false;
+                    comboBox5.Enabled = false;
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.DataBits = Convert.ToInt16(comboBox3.Text);
+
+                    if (comboBox4.Text.Equals("None"))
+                        serialPort1.Parity = System.IO.Ports.Parity.None;
+                    else if (comboBox4.Text.Equals("Odd"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Odd;
+                    else if (comboBox4.Text.Equals("Even"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Even;
+                    else if (comboBox4.Text.Equals("Mark"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Mark;
+                    else if (comboBox4.Text.Equals("Space"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Space;
+
+                    if (comboBox5.Text.Equals("1"))
+                        serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                    else if (comboBox5.Text.Equals("1.5"))
+                        serialPort1.StopBits = System.IO.Ports.StopBits.OnePointFive;
+                    else if (comboBox5.Text.Equals("2"))
+                        serialPort1.StopBits = System.IO.Ports.StopBits.Two;
+
+                    serialPort1.Open();     //打开串口
+                    button1.Text = "关闭串口";
+                    button1.BackColor = Color.Firebrick;
+                }
+            }
+            catch (Exception ex)
+            {
+                //捕获可能发生的异常并进行处理
+
+                //捕获到异常，创建一个新的对象，之前的不可以再用
+                serialPort1 = new System.IO.Ports.SerialPort();
+                //刷新COM口选项
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+                //响铃并显示异常给用户
+                System.Media.SystemSounds.Beep.Play();
+                button1.Text = "打开串口";
+                button1.BackColor = Color.ForestGreen;
+                MessageBox.Show(ex.Message);
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+                comboBox3.Enabled = true;
+                comboBox4.Enabled = true;
+                comboBox5.Enabled = true;
+            }
+        }
+
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -71,6 +151,17 @@ namespace _2105Demo1
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
